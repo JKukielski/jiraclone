d2100fbbcfb0a728b438b712cb5cd31993dcd197
JKukielski <jankukielski1@gmail.com>
2024-11-03 17:40:48 +0100

    Resolving lint errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,14 @@ import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
-    <div>
-      <Button size="sm">Click Me!</Button>
-      <p className="text-red-500 font-semibold">Janek</p>
+    <div className="">
+      <Button>Primary</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="destructive">Destructive</Button>
+      <Button variant="ghost">Ghost</Button>
+      <Button variant="muted">Muted</Button>
+      <Button variant="outline">Outline</Button>
+      <Button variant="teritary">Teritary</Button>
     </div>
   );
 }
